f7f9e598b7ac5aebfd6039bdbb260d95f4d9fef3
Jorge Encarnação <jfgencarnacao@gmail.com>
2018-05-24 14:22:45 +0100

    basic UI done

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ const mapStateToProps  = (state) => ({
 
 const mapDispatchToProps = (dispatch) => {
   return {
+
+    // Dispatch sends the value on #currencySelect to  myReducers()
     cryptChangeHandler: (option) => {
       dispatch({
         type: 'currency',
@@ -20,30 +22,48 @@ const mapDispatchToProps = (dispatch) => {
   }
 };
 
-class CounterView extends Component {
+class DashboardView extends Component {
   render() {
     
     return (
-      <div>
-        <select name="currency" value={this.props.currency} onChange={this.props.cryptChangeHandler}>
-          <option value="" disabled>Please Choose</option>
-          <option value="lsk">Lisk</option>
-          <option value="xmr">Monero</option>
-        </select>
-        <div> here: {this.props.num}</div>
+      <div className="dashboard__tools">
+        <div className="selector">
+          <label className="selector__label" for="currencySelect">Enter Threshold</label>
+          <select className="selector__field" id="currencySelect" name="currencySelect" value={this.props.currency} onChange={this.props.cryptChangeHandler} >
+            <option value="" disabled>Please Choose</option>
+            <option value="eth">Etherium(ETH)</option>
+            <option value="xmr">Monero(XMR)</option>
+            <option value="strat">Stratis(STRAT)</option>
+            <option value="lsk">Lisk(LSK)</option>
+            <option value="bch">Bitcoin Cash(BCH)</option>
+          </select>
+        </div>
+        <div className="threshold">
+          <label className="selector__label" for="threshold">Enter Threshold</label>
+          <input type="text" id="threshold" name="threshold" className="threshold__field" placeholder="eg: BTC00,00" />
+        </div>
       </div>
     )
   }
 }
 
-const CounterViewConnected = connect(mapStateToProps, mapDispatchToProps)(CounterView);
+const DashboardViewConnected = connect(mapStateToProps, mapDispatchToProps)(DashboardView);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <CounterViewConnected /> 
-        app will be here
+      <div className="dashboard">
+        <h1 className="dashboard__title">Cryptocurrency Threshold Evaluator (past 24hrs)</h1>
+        <DashboardViewConnected />
+        // TODO: STYLE THE TABLE
+        <table className="datatable">
+          <tr>
+            <td className="datatable__collumn">time</td>
+            <td className="datatable__collumn">high</td>
+            <td className="datatable__collumn">low</td>
+            <td className="datatable__collumn">volume</td>
+          </tr>
+        </table> 
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { createStore } from 'redux';
 
 import { Provider } from 'react-redux';
 
+
 function myReducers(state = { currency: '' }, action) {
 
     let value;
