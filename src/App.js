a2570d60f4b34a2c0af56aa457c1244658236c54
Jorge Encarnação <jfgencarnacao@gmail.com>
2018-05-27 17:59:19 +0100

    dipping my toes in Auth0 for the first time. Added the auth and callback services, added the router, trying to get login to work

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Auth from './Auth/Auth.js';
 
 import './App.css';
 
@@ -6,11 +7,46 @@ import Selector from './Selector';
 import Threshold from './Threshold';
 import DataTable from './DataTable';
 
+
+const auth = new Auth();
+
 class App extends Component {
+  goTo = (route) => {
+    this.props.history.replace(`/${route}`)
+  }
+
+  login = () => {
+    this.props.auth.login();
+  }
+
+  logout = () => {
+    this.props.auth.logout();
+  }
+
 
   render() {
+    if(this.props.auth) {
+      const { isAuthenticated } = this.props.auth;
+    }
+
     return (
       <div className="dashboard">
+        <div className="topbar">
+          Welcome user,
+                {
+                  !isAuthenticated() && ( 
+                    <button onClick = {() => {this.login()}} >
+                      Log In 
+                    </button>
+                  )
+                } {
+                  isAuthenticated() && ( 
+                    <button onClick = {() => {this.logout()}}>
+                      Log Out
+                    </button>
+                  )
+                }
+        </div>
         <h1 className="dashboard__title">Cryptocurrency Threshold Evaluator (past 24hrs)</h1>
         <div>
           <div className="dashboard__tools">
